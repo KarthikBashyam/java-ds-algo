4b650c2ecc6e0b15e827b77c4bc454a379df1f80
Karthik Bashyam <karthikbashyam@Karthiks-MacBook-Pro.local>
2022-07-01 23:20:01 -0400

    Added subsets problem

--- src/com/practice/strings/Subsets.java
+++ src/com/practice/strings/Subsets.java
@@ -1,5 +1,29 @@
 package com.practice.strings;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Subsets {
 
+	public static void main(String[] args) {
+
+		String str = "abc";
+
+		List<List<String>> subsets = new ArrayList<>();
+		subsets.add(new ArrayList<>());
+
+		for (Character c : str.toCharArray()) {
+			int n = subsets.size();
+
+			for (int i = 0; i < n; i++) {
+				List<String> newSubList = new ArrayList<>(subsets.get(i));
+				newSubList.add(String.valueOf(c));
+				subsets.add(newSubList);
+			}
+
+		}
+		
+		System.out.println(subsets);
+
+	}
 }
